2d50f8ee36d3dba196774b5f284c8c1c9b5f14e7
Sundarcj <sundarcs96@gmail.com>
2018-05-05 23:03:07 +0530

    Server Socket Creation

--- Servlet-git-project/src/com/sundarGit/core/Server.java
+++ Servlet-git-project/src/com/sundarGit/core/Server.java
@@ -1,14 +1,28 @@
 package com.sundarGit.core;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+
 public class Server {
 
+	private final String host = "localhost";
+	private final static int http_port = 8080;
+	private  ServerSocket serverSocket = null;
+	private final int backLog = 50;
 	
-	
-	
+	public  void startServer() {		
+		try {		
+			serverSocket = new ServerSocket(http_port,backLog,InetAddress.getByName(host));
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			System.out.println("Exception in creating server "+e.getMessage());	
+		}		
+	}
+		
 	public  static void main(String []args) {
-
-		
-		
+		new Server().startServer();		
 	}
 	
 }
